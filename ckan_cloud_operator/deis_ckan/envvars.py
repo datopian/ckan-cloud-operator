471594f4d9774f6fb9173fa5fdb4ac2abf688d26
Ori Hoch <ori@uumpa.com>
2019-01-16 09:54:14 +0200

    remove unnecessary print statement

--- ckan_cloud_operator/deis_ckan/envvars.py
+++ ckan_cloud_operator/deis_ckan/envvars.py
@@ -54,7 +54,6 @@ class DeisCkanInstanceEnvvars(object):
             CKANEXT__S3FILESTORE__REGION_NAME=ckan_infra.GCLOUD_STORAGE_REGION_NAME,
             CKANEXT__S3FILESTORE__SIGNATURE_VERSION=ckan_infra.GCLOUD_STORAGE_SIGNATURE_VERSION,
         )
-        print(envvars)
         instance_envvars_secret = {'apiVersion': 'v1',
                                    'kind': 'Secret',
                                    'metadata': {
