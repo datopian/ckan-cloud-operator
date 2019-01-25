bee0fc9135b81f71afc3f8b2f6fb1612321b77b8
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-01-25 18:40:38 +0400

    handle inconsistend storage naming

--- ckan_cloud_operator/deis_ckan/storage.py
+++ ckan_cloud_operator/deis_ckan/storage.py
@@ -16,10 +16,17 @@ class DeisCkanInstanceStorage(object):
 
     def get(self):
         storage_path = self.storage_spec['path']
+        # Some of the srotages are not named after instance, but {instance}-prod/stage etc..
+        prod_paths = [storage_path, f'{storage_path}-prod', f'{storage_path}-production']
+        staging_paths = [storage_path, f'{storage_path}-stage', f'{storage_path}-staging']
         storage_bucket = self.instance.ckan_infra.GCLOUD_STORAGE_BUCKET
-        returncode, output = gcloud.getstatusoutput(f'ls gs://{storage_bucket}{storage_path}',
-                                                    gsutil=True,
-                                                    ckan_infra=self.instance.ckan_infra)
+        for p_path, s_path in zip(prod_paths, staging_paths):
+            storage_path_ = s_path if 'stage' in 'storage_path' else p_path
+            returncode, output = gcloud.getstatusoutput(f'ls gs://{storage_bucket}{storage_path_}',
+                                                        gsutil=True,
+                                                        ckan_infra=self.instance.ckan_infra)
+            if returncode == 0:
+                break
         if returncode == 0:
             return {'ready': True, 'path': storage_path, 'bucket': storage_bucket,
                     'root_paths': [l.strip() for l in output.split('\n')]}
