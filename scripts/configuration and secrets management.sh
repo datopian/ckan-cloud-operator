bfe6543a89a5c689ff0e9d4d64904e59fa411480
Ori Hoch <ori@uumpa.com>
2019-04-17 14:26:32 +0300

    add configuration and secrets management script

---
+++ scripts/configuration and secrets management.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export ACTION="${1}"
+export ARGS="${2}"
+! python3 -c "exit(0 if '${ACTION}' in [
+    'get', 'set', 'list-configs', 'kubectl get secret'
+] else 1)" && echo invalid ACTION && exit 1
+
+if [ "${ACTION}" == "kubectl get secret" ]; then
+	ckan-cloud-operator kubectl -- get secret "$ARGS"
+else
+	ckan-cloud-operator config "${ACTION}" "$ARGS"
+fi
