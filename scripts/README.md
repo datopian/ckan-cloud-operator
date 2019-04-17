4a7a0182a35dad07a7a2c0dd998cbd5443cbdcd6
Ori Hoch <ori@uumpa.com>
2019-04-17 14:13:27 +0300

    add cluster resources datapackage script

---
+++ scripts/README.md
@@ -0,0 +1,3 @@
+# ckan-cloud-operator scripts
+
+misc. scripts which are typically executed from an automation app like Jenkins.
---
+++ scripts/cluster resources datapackage.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+DATAPACKAGE_PREFIX="${1}"
+NON_CKAN_INSTANCES="${2}"
+[ -z "${DATAPACKAGE_PREFIX}" ] && echo invalid args && exit 1
+echo DATAPACKAGE_PREFIX=$DATAPACKAGE_PREFIX
+echo NON_CKAN_INSTANCES=$NON_CKAN_INSTANCES
+
+rm -rf .checkpoints &&\
+rm -rf data/$DATAPACKAGE_PREFIX &&\
+python3 /home/jenkins/ckan-cloud-operator/ckan_cloud_operator/dataflows/resources.py &&\
+python3 /home/jenkins/ckan-cloud-operator/ckan_cloud_operator/dataflows/ckan_images.py &&\
+python3 -c "
+from dataflows import Flow, load, printer
+Flow(
+  load('data/ckan-cloud-prod/resources/datapackage.json'),
+  printer(tablefmt='html', num_rows=9999)
+).process()
+" > resources.html
+python3 -c "
+from dataflows import Flow, load, printer
+Flow(
+  load('data/ckan-cloud-prod/ckan_images/datapackage.json'),
+  printer(resources=['dockerfiles'], tablefmt='html', num_rows=9999, fields=['gitlab_repo','name', 'url', 'instances','from','ckan_exts','ckanext-s3filestore'])
+).process()
+" > dockerfiles.html
