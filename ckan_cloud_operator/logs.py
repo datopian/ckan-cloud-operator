008c078d28a8c0461d3229ed516ad33295b0f682
Mikhail Mezyakov <mihail265@gmail.com>
2019-12-18 15:30:47 +0400

    Python 3.6 compatibility

--- ckan_cloud_operator/logs.py
+++ ckan_cloud_operator/logs.py
@@ -74,7 +74,7 @@ def log_subprocess_output(stdout, stderr):
 def subprocess_run(command, input=None):
     completed = subprocess.run(
         command, input=input, 
-        shell=True, check=True, capture_output=True
+        shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
     log_subprocess_output(completed.stdout, completed.stderr)
 
