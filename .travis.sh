bc2f4f7c249abff97f2ce19085d80f34b543ddba
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2019-12-21 09:50:48 +0400

    instal azure cli from apt

--- .travis.sh
+++ .travis.sh
@@ -27,7 +27,16 @@ elif [ "${1}" == "install-tools" ]; then
       echo Terraform Installed Successfully!
 
       # Intall Azure CLI and login
-      curl -L https://aka.ms/InstallAzureCli | bash
+      sudo apt-get update
+      sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+      curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+      gpg --dearmor |
+      sudo tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
+      AZ_REPO=$(lsb_release -cs)
+      echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+      sudo tee /etc/apt/sources.list.d/azure-cli.list
+      sudo apt-get update
+      sudo apt-get install azure-cli
       echo Azure CLI Installed Successfully!
    fi
     curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
