4438b0e677f738d278f18ea4a38fa05cd9190796
Adam Kariv <adam.kariv@gmail.com>
2020-01-02 12:27:21 +0200

    Build AMI on Travis

---
+++ terraform/aws/init_cluster.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 5 ]; then
+    cat << USAGE
+
+Initialize AWS CCO Cluster using terraform and ckan-cloud-operator
+
+Usage:
+${0} <aws-access-key-id> <aws-secret-access-key> <aws-region> <vpc-id> <cluster-name>
+
+USAGE
+    exit 1
+fi
+
+./terraform init -input=false
+./terraform validate
+
+export TF_VAR_aws_access_key_id="${1}"
+export TF_VAR_aws_secret_access_key="${2}"
+export TF_VAR_region="${3}"
+export TF_VAR_vpc_id="${4}"
+export TF_VAR_cluster_name="${5}"
+./terraform apply -input=false -auto-approve
+./terraform output cco-interactive-yaml > interactive.yaml
+
+export CCO_INTERACTIVE_CI=interactive.yaml
+export KUBECONFIG="kubeconfig_${TF_VAR_cluster_name}"
+ckan-cloud-operator cluster initialize --interactive --cluster-provider=aws
