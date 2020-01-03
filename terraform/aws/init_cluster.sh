c3d031d700ceeefaafd081617ec991f4a93d2406
Adam Kariv <adam.kariv@gmail.com>
2020-01-03 08:49:53 +0200

    Add external domain as a parameter

--- terraform/aws/init_cluster.sh
+++ terraform/aws/init_cluster.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 
-if [ "$#" -ne 5 ]; then
+if [ "$#" -ne 6 ]; then
     cat << USAGE
 
 Initialize AWS CCO Cluster using terraform and ckan-cloud-operator
 
 Usage:
-${0} <aws-access-key-id> <aws-secret-access-key> <aws-region> <vpc-id> <cluster-name>
+${0} <aws-access-key-id> <aws-secret-access-key> <aws-region> <vpc-id> <cluster-name> <external-domain>
 
 USAGE
     exit 1
@@ -21,6 +21,7 @@ export TF_VAR_aws_secret_access_key="${2}"
 export TF_VAR_region="${3}"
 export TF_VAR_vpc_id="${4}"
 export TF_VAR_cluster_name="${5}"
+export TF_VAR_external_domain="${6}"
 ./terraform apply -input=false -auto-approve
 ./terraform output cco-interactive-yaml > interactive.yaml
 
