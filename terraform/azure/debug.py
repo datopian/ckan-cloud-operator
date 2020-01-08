4abd15d189c7b99b790fa5af6474a07b45fe1dbd
zelima <ir.mchedlishvili@gmail.com>
2020-01-08 14:22:11 +0400

    try manually writing azureprofile.json

---
+++ terraform/azure/debug.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+template = {
+    "installationId": os.environ.get('AZ_TEST_installationId'),
+    "subscriptions": [
+        {
+        "id": os.environ.get('AZ_TEST_id'),
+        "name": "Datopian",
+        "state": "Enabled",
+        "user": {
+            "name": os.environ.get('AZ_TEST_email'),
+            "type": "user"
+        },
+        "isDefault": True,
+        "tenantId": os.environ.get('AZ_TEST_tenantId'),
+        "environmentName": "AzureCloud"
+        }
+    ]
+}
+
+with open(os.path.expanduser('~/.azure/test.json'), 'w') as outfile:
+    json.dump(template, outfile)
