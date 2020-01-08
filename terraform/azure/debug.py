355f7081f4ac40755d56b7dfdf170e1158020e24
zelima <ir.mchedlishvili@gmail.com>
2020-01-08 10:43:25 +0400

    list files in azure directory

--- terraform/azure/debug.py
+++
@@ -1,7 +0,0 @@
-import json
-import os
-import codecs
-
-prof = json.loads(codecs.open(os.path.expanduser('~/.azure/azureProfile.json'), 'r', 'utf-8-sig').read())
-print('========================================')
-print(prof.get('subscriptions', [{}])[0].get('id', '1234')[-4:])
