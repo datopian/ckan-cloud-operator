fba3a3e782b717f508a5d152179196553f2dd1c1
Adam Kariv <adam.kariv@gmail.com>
2020-01-15 20:19:46 +0200

    Add vpc-id to terraform destroy

--- terraform/aws/destroy_aws.sh
+++ terraform/aws/destroy_aws.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 for vid in `aws ec2 describe-volumes | jq '.Volumes[] | .VolumeId' -r` ; do echo $vid ; aws ec2 delete-volume --volume-id "$vid"; done
 for lb in `aws elb describe-load-balancers | jq '.LoadBalancerDescriptions[] | .LoadBalancerName' -r` ; do echo $lb ; aws elb delete-load-balancer --load-balancer-name "$lb"; done
+export TF_VAR_vpc_id="vpc-30aedd58"
 ./terraform destroy -auto-approve
