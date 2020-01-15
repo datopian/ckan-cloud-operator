c056f3556d22a29389037691c66bed77273fd3a6
Adam Kariv <adam.kariv@gmail.com>
2020-01-15 23:04:17 +0200

    Default region to AWS destroy script

--- terraform/aws/destroy_aws.sh
+++ terraform/aws/destroy_aws.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+export AWS_DEFAULT_REGION=eu-west-2
+export TF_VAR_vpc_id="vpc-30aedd58"
 for vid in `aws ec2 describe-volumes | jq '.Volumes[] | .VolumeId' -r` ; do echo $vid ; aws ec2 delete-volume --volume-id "$vid"; done
 for lb in `aws elb describe-load-balancers | jq '.LoadBalancerDescriptions[] | .LoadBalancerName' -r` ; do echo $lb ; aws elb delete-load-balancer --load-balancer-name "$lb"; done
-export TF_VAR_vpc_id="vpc-30aedd58"
 ./terraform destroy -auto-approve
