f76c21f4bfbd2a6f8e38fd3254834f3118808ff1
zelima <ir.mchedlishvili@gmail.com>
2020-01-15 16:26:52 +0400

    remove obsolete script

--- helm_init.sh
+++
@@ -1,7 +0,0 @@
-helm init
-sleep 10
-helm repo update
-slee 20
-helm install --namespace=ckan-cloud stable/nfs-server-provisioner --name cloud-nfs --set=persistence.enabled=true,persistence.size=500Gi,storageClass.name=cca-ckan
-sleep 200
-kubectl create -f cca-storage.yaml
