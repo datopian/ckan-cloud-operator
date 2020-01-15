8e1179edfd600b653ce6aa741a50c35d902a3475
Mikhail Mezyakov <mihail265@gmail.com>
2020-01-15 14:40:04 +0400

    Filter out persistent buckets

--- terraform/aws/destroy_aws.sh
+++ terraform/aws/destroy_aws.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 for vid in `aws ec2 describe-volumes | jq '.Volumes[] | .VolumeId' -r` ; do echo $vid ; aws ec2 delete-volume --volume-id "$vid"; done
 for lb in `aws elb describe-load-balancers | jq '.LoadBalancerDescriptions[] | .LoadBalancerName' -r` ; do echo $lb ; aws elb delete-load-balancer --load-balancer-name "$lb"; done
-aws s3 ls | cut -d" " -f 3 | xargs -I{} aws s3 rb s3://{}
+aws s3 ls | cut -d" " -f 3 | grep -v persistent | xargs -I{} aws s3 rb s3://{}
 ./terraform destroy -auto-approve
