425fede8d077dac7ebebfdbb6f071ed9d85b3de1
Adam Kariv <adam.kariv@gmail.com>
2020-01-16 13:29:44 +0200

    Only delete owned LBs #aws

--- terraform/aws/destroy_aws.sh
+++ terraform/aws/destroy_aws.sh
@@ -2,5 +2,5 @@
 export AWS_DEFAULT_REGION=eu-west-2
 export TF_VAR_vpc_id="vpc-30aedd58"
 for vid in `aws ec2 describe-volumes | jq '.Volumes[] | .VolumeId' -r` ; do echo $vid ; aws ec2 delete-volume --volume-id "$vid"; done
-for lb in `aws elb describe-load-balancers | jq '.LoadBalancerDescriptions[] | .LoadBalancerName' -r` ; do echo $lb ; aws elb delete-load-balancer --load-balancer-name "$lb"; done
+for lb in `aws elb describe-load-balancers | jq '.LoadBalancerDescriptions[] | .LoadBalancerName + " " + .VPCId' -r | grep vpc-30aedd58 | cut -d' ' -f1` ; do echo $lb ; aws elb delete-load-balancer --load-balancer-name "$lb"; done
 ./terraform destroy -auto-approve
