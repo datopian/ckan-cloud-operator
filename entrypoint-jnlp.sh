cfc5e5e1f14e606e02b64787eb09a35be34ce803
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2021-02-03 14:29:09 +0400

    Allow skipping getting specific kubeconfig

--- entrypoint-jnlp.sh
+++ entrypoint-jnlp.sh
@@ -4,12 +4,15 @@
 ! [ -e /etc/ckan-cloud/.kube-config ] && echo missing /etc/ckan-cloud/.kube-config && exit 1
 ! [ -z "${KUBE_CONTEXT}" ] && kubectl config use-context "${KUBE_CONTEXT}" >/dev/null 2>&1
 export KUBECONFIG=/etc/ckan-cloud/.kube-config
+
+if [ -z "${SKIP_GET_KUBECONFIG}" ]; then
 USER_KUBECONFIG=`mktemp`
 if ! [ "$(ckan-cloud-operator config get --key=ckan-cloud-provider-cluster-main-provider-id --raw)" == "aws" ]; then
   ckan-cloud-operator activate-gcloud-auth >/dev/null 2>&1
 fi &&\
 ckan-cloud-operator users get-kubeconfig "${CKAN_CLOUD_USER_NAME}" > "${USER_KUBECONFIG}"
 [ "$?" != "0" ] && echo failed to get authentication credentials && exit 1
+fi
 
 rm -rf .config/gcloud
 
