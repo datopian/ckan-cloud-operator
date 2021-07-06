c6352335c2d05d621bb9ba8b442d2ac5d919baae
Irakli Mchedlishvili <you@example.com>
2021-07-06 10:27:33 +0400

    remove more jenkins

--- .travis.sh
+++ .travis.sh
@@ -9,7 +9,6 @@ HELM_VERSION="${HELM_VERSION:-v3.5.2}"
 
 if [ "${1}" == "install" ]; then
     ! docker pull viderum/ckan-cloud-operator:latest && echo Failed to pull image && exit 1
-    # ! docker pull viderum/ckan-cloud-operator:jnlp-latest && echo Failed to pull jnlp image && exit 1
     echo Great Success! && exit 0
 
 elif [ "${1}" == "install-tools" ]; then
@@ -111,26 +110,11 @@ elif [ "${1}" == "deploy" ]; then
     echo && echo "viderum/ckan-cloud-operator:${TAG}" && echo &&\
     docker push "viderum/ckan-cloud-operator:${TAG}"
     [ "$?" != "0" ] && echo Failed to tag and push && exit 1
-    docker tag ckan-cloud-operator-jnlp "viderum/ckan-cloud-operator:jnlp-${TAG}" &&\
-    echo && echo "viderum/ckan-cloud-operator:jnlp-${TAG}" && echo &&\
-    docker push "viderum/ckan-cloud-operator:jnlp-${TAG}"
-    [ "$?" != "0" ] && echo Failed to tag and push jnlp image && exit 1
     if [ "${GITHUB_REF}" == "master" ]; then
         docker tag ckan-cloud-operator viderum/ckan-cloud-operator:latest &&\
         echo && echo viderum/ckan-cloud-operator:latest && echo &&\
         docker push viderum/ckan-cloud-operator:latest
         [ "$?" != "0" ] && echo Failed to tag and push latest image && exit 1
-        docker tag ckan-cloud-operator-jnlp viderum/ckan-cloud-operator:jnlp-latest &&\
-        echo && echo viderum/ckan-cloud-operator:jnlp-latest && echo &&\
-        docker push viderum/ckan-cloud-operator:jnlp-latest
-        [ "$?" != "0" ] && echo Failed to tag and push jnlp latest image && exit 1
-    fi
-    if [ "${TRAVIS_TAG}" != "" ]; then
-        export DEPLOY_JNLP_IMAGE="viderum/ckan-cloud-operator:jnlp-${TAG}"
-        echo "Running Jenkins deploy jnlp job (JNLP_IMAGE=${DEPLOY_JNLP_IMAGE})"
-        STATUS_CODE=$(curl -X POST "${JENKINS_JNLP_DEPLOY_URL}" --user "${JENKINS_USER}:${JENKINS_TOKEN}" --data "JNLP_IMAGE=${DEPLOY_JNLP_IMAGE}" -s -o /dev/stderr -w "%{http_code}")
-        echo "jenkins jnlp deploy job status code: ${STATUS_CODE}"
-        [ "${STATUS_CODE}" != "200" ] && [ "${STATUS_CODE}" != "201" ] && echo Deploy failed && exit 1
     fi
     echo Great Success! && exit 0
 
