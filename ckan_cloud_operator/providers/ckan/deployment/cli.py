f3588f0a7fff4a3b08c7d5a5809955d01d51e7bc
Irakli Mchedlishvili <you@example.com>
2021-08-10 10:44:25 +0400

    Introduce deployments status

--- ckan_cloud_operator/providers/ckan/deployment/cli.py
+++ ckan_cloud_operator/providers/ckan/deployment/cli.py
@@ -3,7 +3,7 @@ import click
 from .helm import cli as helm_cli
 from .drone import cli as drone_cli
 from .drone import manager
-
+from ckan_cloud_operator.drivers.helm import driver as helm_driver
 
 drone_manager = manager.Drone()
 
@@ -19,6 +19,12 @@ def logs(branch):
     drone_manager.initialize()
     drone_manager.builds_logs(branch)
 
+
+@deployment.command()
+@click.argument('instance-id')
+def status(instance_id):
+    """See CKAN instances deployment Logs"""
+    helm_driver.check_status(instance_id)
 
 deployment.add_command(helm_cli.helm)
 deployment.add_command(drone_cli.drone)
